28b1dbd2570628c95ce8268e1b6840e3409f3f1f
First Last <robee.dev@gmail.com>
2020-03-02 21:31:43 +0200

    Fixing charts

--- frontend/src/components/Charts.component.js
+++ frontend/src/components/Charts.component.js
@@ -10,19 +10,28 @@ class ChartsComponent extends Component {
       return (<p>Waiting...</p>);
     }
 
-    const parsedCandles = this.props.candles.map(candle => {
-      let obj = {};
-      obj.date = moment(candle.date).toDate();
-      obj.open = candle.open;
-      obj.high = candle.high;
-      obj.low = candle.low;
-      obj.close = candle.close;
-      obj.volume = candle.volume;
-      return obj;
-    })
+    const allParsedCandles = this.props.candles.map(candleObj => {
+      let parsedCandles = candleObj.candles.map(candle => {
+        let obj = {};
+        obj.date = moment(candle.date).toDate();
+        obj.open = candle.open;
+        obj.high = candle.high;
+        obj.low = candle.low;
+        obj.close = candle.close;
+        obj.volume = candle.volume;
+        return obj;
+      });
+      return parsedCandles;
+    });
+
+    const charts = allParsedCandles.map(candles => {
+      return <Chart type='hybrid' data={candles} />
+    });
 
     return (
-      <Chart type='hybrid' data={parsedCandles} />
+      <div>
+        {charts}
+      </div>
     )
   }
 }
